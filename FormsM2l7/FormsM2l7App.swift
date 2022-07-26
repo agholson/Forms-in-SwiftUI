12841d5b7c57eb2345f869932fa73a4502d84172
agholson <87455899+agholson@users.noreply.github.com>
2022-07-26 17:15:24 -0400

    Initial Commit

---
+++ FormsM2l7/FormsM2l7App.swift
@@ -0,0 +1,17 @@
+//
+//  FormsM2l7App.swift
+//  FormsM2l7
+//
+//  Created by Leone on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FormsM2l7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
